3322ce69800edd84311b9c7a29d6dbbc65d3517c
Katie Foster <inkblotty@github.com>
2022-06-02 03:15:14 +0000

    starting tests

---
+++ src/groupPRs.test.ts
@@ -0,0 +1,29 @@
+import * as prData from '../fixtures/getAllWorkResponse.fixture.json';
+
+const mockGraphQlFunction = jest.fn();
+jest.mock('@octokit/graphql', () => ({
+    graphql: mockGraphQlFunction
+}));
+
+import { getAllWorkForRepository } from './queries';
+import handlePRGroups from './groupPRs';
+import { QueryGroup } from './shared.types';
+
+interface MockData {
+    prsCreated: QueryGroup[];
+    prComments: QueryGroup[];
+    prCommits: QueryGroup[];
+}
+let mockData: MockData = {};
+
+describe('groupPRs', () => {
+    beforeAll(async () => {
+        mockGraphQlFunction.mockResolvedValue(prData);
+        const result = await getAllWorkForRepository('github', 'accessibility', 'inkblotty', '2021-12-01');
+        mockData = result;
+    });
+
+    test('handlePRGroups', async () => {
+        const result = await handlePRGroups(mockData.prsCreated, mockData.prComments, mockData.prCommits);
+    });
+});
