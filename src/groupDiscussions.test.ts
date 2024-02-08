cf018377f6c75744e33e7194b833d90b17390bad
Katie McCormick <inkblotty@github.com>
2024-02-08 18:59:37 +0000

    start setting up grouping and tests

---
+++ src/groupDiscussions.test.ts
@@ -0,0 +1,38 @@
+import * as discussionData from '../fixtures/getAllWorkResponse.fixture.json';
+
+const mockGraphQlFunction = jest.fn();
+jest.mock('@octokit/graphql', () => ({
+    graphql: mockGraphQlFunction
+}));
+
+import { getAllWorkForRepository } from './queries';
+import handleDiscussionGroups from './groupDiscussions';
+import { QueryGroup } from './shared.types';
+
+interface MockData {
+    discussionsCreated: QueryGroup;
+    discussionComments: QueryGroup;
+}
+const mockData: MockData = {
+    discussionsCreated: {} as QueryGroup,
+    discussionComments: {} as QueryGroup,
+};
+
+describe('groupDiscussions', () => {
+    beforeAll(async () => {
+        mockGraphQlFunction.mockResolvedValue(discussionData);
+        const result = await getAllWorkForRepository('github', 'accessibility', 'inkblotty', '2021-12-01');
+        mockData.discussionComments = result.discussionComments;
+        mockData.discussionsCreated = result.discussionsCreated;
+    });
+
+    test('handleDiscussionGroups', async () => {
+        const result = await handleDiscussionGroups([mockData.discussionsCreated], [mockData.discussionComments]);
+
+        expect(result.primary['github/accessibility']).toBeTruthy();
+        expect(result.primary['github/accessibility'].artifacts.length).toEqual(50);
+
+        // number of discussions commented on by inkblotty since Dec 1, 2021
+        expect(Object.keys(result.secondary).length).toEqual(37);
+    });
+});
---
+++ src/groupDiscussions.ts
@@ -0,0 +1,56 @@
+import { log } from "console";
+import { OutputGroup, OutputGroupGroup, QueryGroup, QueryType } from "./shared.types";
+
+const handleDiscussionGroups = (allDiscussionsCreated: QueryGroup[], allDiscussionComments: QueryGroup[]): OutputGroupGroup => {
+    const finalDiscussions: OutputGroupGroup = {
+        // primary, meaning directly authored
+        primary: {
+        },
+        // secondary, meaning contributed to or reviewed
+        secondary: {
+        },
+        // unknown, meaning the commits are unattached to a PR
+        unknown: {
+        }
+    };
+
+    for (const repoGroup of allDiscussionsCreated) {
+
+        const { data, type } = repoGroup;
+        if (type === QueryType['discussion-created'] && data[0]) {
+            const [repoUrl] = data[0].url.split('/discussions');
+            const [_, repoName] = repoUrl.split('github.com/');
+
+            finalDiscussions.primary[repoName] = {
+                groupTitle: `Discussions Created in [${repoName}](${repoUrl})`,
+                itemType: 'Discussion',
+                artifacts: data.map(discussion => ({
+                    title: discussion.title,
+                    url: discussion.url,
+                }))
+            }
+        }
+    };
+
+    for (const repoGroup of allDiscussionComments) {
+        for (const comment of repoGroup.data) {
+            const discussionUrl = comment.url.split('#')[0];
+
+            // make sure that comment belongs to a group
+            if (!finalDiscussions.secondary[discussionUrl]) {
+                finalDiscussions.secondary[discussionUrl] = {
+                    itemType: 'DiscussionComment',
+                    groupTitle: `Left comments on discussion: [${comment.discussion.title}](${discussionUrl})`,
+                    artifacts: [],
+                }
+            }
+
+            finalDiscussions.secondary[discussionUrl].artifacts.push({
+                title: `Comment #${finalDiscussions.secondary[discussionUrl].artifacts.length + 1}`,
+                url: comment.url,
+            });
+        }
+    };
+    return finalDiscussions as OutputGroupGroup;
+}
+export default handleDiscussionGroups;
