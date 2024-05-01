12d5d40a188d4dc7e9e9c2df55379e828a9a674e
Kate Travers <8152930+ktravers@users.noreply.github.com>
2024-05-01 19:47:31 +0000

    use REST API to add assignee to newly created PR

--- src/openPR.ts
+++ src/openPR.ts
@@ -5,6 +5,8 @@ import { InputFields } from "./shared.types";
 const openPR = async ({ owner, repo }: InputFields, username: string, branchName: string, body: string, destinationBranch: string): Promise<{ html_url: string }> => {
     const now = formatDate(new Date());
     const requestOwner = repo.includes('/') ? repo.split('/')[0] : owner;
+    const octokit = github.getOctokit(process.env.GH_TOKEN);
+
     const prData = {
         owner: requestOwner,
         repo,
@@ -17,7 +19,21 @@ const openPR = async ({ owner, repo }: InputFields, username: string, branchName
             authorization: `token ${process.env.GH_TOKEN}`
         },
     };
-    const { data } = await github.getOctokit(process.env.GH_TOKEN).request('POST /repos/{owner}/{repo}/pulls', prData);
+    const { data } = await octokit.request('POST /repos/{owner}/{repo}/pulls', prData);
+
+    const assigneeData = {
+        owner: requestOwner,
+        repo,
+        issue_number: data.number,
+        assignees: [
+            username
+        ],
+        headers: {
+            authorization: `token ${process.env.GH_TOKEN}`
+        }
+    }
+    octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/assignees', assigneeData)
+
     return { html_url: data.html_url };
 }
 export default openPR;
